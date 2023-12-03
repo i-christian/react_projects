4ecfd495c5dd24648fda3a496f0cb91be5811a89
Christian <111076892+i-christian@users.noreply.github.com>
2023-12-03 15:42:19 +0200

    update NewPost.jsx

--- React_Blog/src/components/NewPost.jsx
+++ React_Blog/src/components/NewPost.jsx
@@ -1,9 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState} from "react";
 import DataContext from "../context/DataContext";
+import {format} from 'date-fns'
+import { useNavigate } from "react-router-dom";
+import api from '../api/posts'
 
 
 const NewPost = () => {
-    const {handleSubmit, postTitle, setPostTitle, postBody, setPostBody} = useContext(DataContext);
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
+    const {posts, setPosts} = useContext(DataContext);
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const id = posts.length ? posts[posts.length - 1].id + 1: 1;
+        const datetime = format(new Date(), 'MMMM dd, yyyy pp')
+        const newPost = {id, title: postTitle, datetime, body: postBody};
+         
+        try {
+          const response = await api.post('/posts', newPost);
+          const allPosts = [...posts, response.data];
+          setPosts(allPosts);
+          setPostTitle('');
+          setPostBody('');
+          navigate('/');
+        }catch (err) {
+          console.log(`Error: ${err.message}`)
+        }
+      }
+
     return(
         <main className="grow w-full bg-slate-700 rounded-md p-4 text-xl">
             <h1 className="text-2xl text-center p-4"> New Post</h1>
