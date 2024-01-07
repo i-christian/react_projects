60dd0a646d898617bef65b0513bb2d5a3fe100a4
Christian <111076892+i-christian@users.noreply.github.com>
2024-01-08 01:56:10 +0200

    update Login page

--- React_Blog/client/src/App.tsx
+++ React_Blog/client/src/App.tsx
@@ -2,16 +2,46 @@ import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
 } from "react-router-dom"
 import Home from './pages/Home'
 import Register from "./pages/Register"
 import Login from "./pages/Login"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 import Single from "./pages/Single"
+import Write from "./pages/Write"
+
+
+const Layout = () => {
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      }
+    ]
   },
   {
     path: "/register",
@@ -26,9 +56,11 @@ const router = createBrowserRouter([
 
 const App = () => {
   return (
-    <>
-      <RouterProvider router={router}/>
-    </>
+    <main className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
+    </main>
   )
 }
 
--- React_Blog/client/src/pages/Login.tsx
+++ React_Blog/client/src/pages/Login.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
 
-import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="auth">
+        <h1>Login</h1>
+        <form>
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+            <p>This is an error</p>
+            <span>Don't have an account? <Link to="/register">Register</Link></span>
+        </form>
+    </section>
   )
 }
 
