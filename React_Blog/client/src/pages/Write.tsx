436d4d6c0396bfece3017d26cde190cd00491981
Christian <111076892+i-christian@users.noreply.github.com>
2024-01-27 16:57:08 +0200

    lets start the backend

--- React_Blog/client/src/pages/Write.tsx
+++ React_Blog/client/src/pages/Write.tsx
@@ -19,7 +19,7 @@ const Write = () => {
             <b>Visibility: </b>Public
           </span>
           <input type='file' id='file' name=''/>
-          <label htmlFor='file'>Upload Image</label>
+          <label className="file" htmlFor='file'>Upload Image</label>
           <div className='buttons'>
             <button>Save as a draft</button>
             <button>Update</button>
@@ -27,18 +27,36 @@ const Write = () => {
         </div>
         <div className="item">
           <h1>Category</h1>
-          <input type='radio' name='cat' value="art" id="art" />
-          <label htmlFor='art'>Art</label>
-          <input type='radio' name='cat' value="science" id="science" />
-          <label htmlFor='science'>Science</label>
-          <input type='radio' name='cat' value="technology" id="technology" />
-          <label htmlFor='technology'>Technology</label>
-          <input type='radio' name='cat' value="cinema" id="cinema" />
-          <label htmlFor='cinema'>Cinema</label>
-          <input type='radio' name='cat' value="design" id="design" />
-          <label htmlFor='design'>Design</label>
-          <input type='radio' name='cat' value="food" id="food" />
-          <label htmlFor='food'>Food</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value="art" id="art" />
+            <label htmlFor='art'>Art</label>
+          </div>
+
+          <div className="cat">
+            <input type='radio' name='cat' value="science" id="science" />
+            <label htmlFor='science'>Science</label>
+          </div>
+
+          <div className="cat">
+            <input type='radio' name='cat' value="technology" id="technology" />
+            <label htmlFor='technology'>Technology</label>
+          </div>
+
+          <div className='cat'>
+            <input type='radio' name='cat' value="cinema" id="cinema" />
+            <label htmlFor='cinema'>Cinema</label>
+          </div>
+
+          <div className='cat'>
+            <input type='radio' name='cat' value="design" id="design" />
+            <label htmlFor='design'>Design</label>
+          </div>
+
+          <div className="cat">
+            <input type='radio' name='cat' value="food" id="food" />
+            <label htmlFor='food'>Food</label>
+          </div>
+          
         </div>
       </div>
     </section>
