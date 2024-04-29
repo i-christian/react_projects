9b3e9594bd2a9665fe10e5d02b088f59c402cfe6
Christian <111076892+i-christian@users.noreply.github.com>
2024-04-29 15:05:35 +0200

    updater controllers/auth.ts

--- blog/api/src/controllers/auth.ts
+++ blog/api/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import { db } from "../db";
+import jwt from "jsonwebtoken"
 
 export const register = (req: express.Request, res: express.Response) => {
   // check existing user
@@ -31,8 +32,32 @@ export const register = (req: express.Request, res: express.Response) => {
 
 export const login = (req: express.Request, res: express.Response) => {
   // Implement login logic here
-};
+  const selectQuery = "SELECT * FROM users WHERE username = ? "
+
+  //Check the username
+  db.query(selectQuery, [req,body,name], (err,data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found");
+
+  //Check the password
+  const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+  if (!isPasswordCorrect) return res.status(400).json("Wrong username or password");
+
+  const token = jwt.sign({ id: data[0].id }, "jwtkey");
+  const { password, ...other } = data[0];
+
+  res.cookie("access_token", token, {
+        httpOnly: true,
+      }).status(200).json(other);
+
+  })
+}
 
 export const logout = (req: express.Request, res: express.Response) => {
   // Implement logout logic here
+    res.clearCookie("access_token",{
+    sameSite:"none",
+    secure:true
+  }).status(200).json("User has been logged out.")
 };
